fe620ae8176fd27faa0eadf1d063da816d2351f7
Rahul6954 <rahulpmali2004@gmail.com>
2024-10-02 16:31:54 +0530

    Set Url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,12 @@ import { Register } from './pages/Register'
 import { ForgotPassword } from './pages/Password-Reset/ForgotPassword'
 import { ResetPassword } from './pages/Password-Reset/ResetPassword'
 import { Header } from './components/Header'
-import { CskAllItems, CreateClient, ClientTrash, ClientFavroite } from './pages/Client'
+import {
+  CskAllItems,
+  CreateClient,
+  ClientTrash,
+  ClientFavroite
+} from './pages/Client'
 import { CreateSite, SiteAllItems, SiteFavroite, SiteTrash } from './pages/site'
 import { Logout } from './pages/Logout'
 
@@ -20,7 +25,10 @@ const App = () => {
           <Route path='/register' element={<Register />} />
           <Route path='/logout' element={<Logout />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
-          <Route path='/reset-password/:emailId/:token' element={<ResetPassword />} />
+          <Route
+            path='/reset-password/:emailId/:token'
+            element={<ResetPassword />}
+          />
           <Route path='/client/all-items' element={<CskAllItems />} />
           <Route path='/client/trash' element={<ClientTrash />} />
           <Route path='/client/fav' element={<ClientFavroite />} />
